e5ee3ceb45cc10dd43ee3ffc602cf4d5c3aa42bf
yun27jin <yunjin_jang@wemakeprice.com>
2022-01-07 19:23:52 +0900

    spring scheduler 적용

---
+++ src/main/java/com/example/demo/SampleScheduler.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SampleScheduler {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    @Scheduled(fixedDelay = 1000)
+    public void schedule() {
+        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+        jobParametersBuilder.addParameter("currenteDateTime", new JobParameter(LocalDateTime.now().toString()));
+        try {
+            jobLauncher.run(job, jobParametersBuilder.toJobParameters());
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (JobRestartException e) {
+            e.printStackTrace();
+        } catch (JobInstanceAlreadyCompleteException e) {
+            e.printStackTrace();
+        } catch (JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+}
